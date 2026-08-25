1beb5a6251e7a12093b0d5c9e730c4186dfa5ec0
Lucca Zogbi <originlucca@hotmail.com>
2026-08-25 08:42:13 -0300

    test: configure pytest and add price level insertion test
    create some examples to run and verify if everything was done well inside order.py and price_level.py

--- tests/test_price_level.py
+++ tests/test_price_level.py
@@ -1,3 +1,27 @@
-from matching_engine.order import Order
+from decimal import Decimal
+
+from matching_engine.order import Order, Side, OrderType
 from matching_engine.price_level import PriceLevel
+
+def test_insert_orders(): 
+    level = PriceLevel(Decimal(10.50))
+    a = Order(side=Side.BUY, order_type=OrderType.LIMIT, qty=100, price=Decimal(10.50))
+    b = Order(side=Side.BUY, order_type=OrderType.LIMIT, qty=200, price=Decimal(10.50))
+    c = Order(side=Side.BUY, order_type=OrderType.LIMIT, qty=50, price=Decimal(10.50))
+
+    level.last_insert(a)
+    level.last_insert(b)
+    level.last_insert(c)
+
+    # Tests
+    assert level.first is a
+    assert level.last is c
+    assert level.total_qty == 350
+
+    assert a.next_order is b
+    assert b.previous_order is a
+    assert b.next_order is c
+    assert c.previous_order is b
+
+
 
