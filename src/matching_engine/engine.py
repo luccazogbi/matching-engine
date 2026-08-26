16af7c90a68c6c9b68dc9fe9aca7f93f0c04b282
Lucca Zogbi <originlucca@hotmail.com>
2026-08-26 12:12:36 -0300

    feat: implement limit order matching
    implement all the process when we're talking about limit orders. You can create its level if there's no liquidity in the other side, the price isn't acceptable or the matching was done. Furthermore, all the process to do this match is implemented, considering the FIFO queue. So, if you want to match with the acceptable opposite price level, you need to pass through all the orders inside of it, following the order of arrivel. The method responsible for doing that returns a trades list, containing all the trades and a peculiarity is that trades are agregates by price level. So you can't see each execution against a passive order (it was already in the book). In real-world scenario it'd be different, but to match with received project e-mail, we're going to do in this way.

--- src/matching_engine/engine.py
+++ src/matching_engine/engine.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, field
 from decimal import Decimal
+from .order_book import OrderBook
+from .order import Side, Order, OrderType, PegReference
+from .price_level import PriceLevel 
 
 @dataclass
 class Trade:
@@ -9,3 +12,85 @@ class Trade:
 
     def __str__(self) -> str:
         return f"Trade, price: {self.price.normalize():f}, qty: {self.qty}"
+
+class MatchingEngine:
+
+    def __init__(self):
+        self.book = OrderBook()
+
+    # It'll submit the limit order
+    def submit_limit(self,
+            side: Side,
+            price: Decimal,
+            qty: int
+    ) -> list[Trade]:
+
+        # Create the limit order (aggresive)
+        order = Order(side=side, order_type=OrderType.LIMIT, qty=qty, price=price)
+        list_trades = []
+
+        while order.qty != 0: 
+
+            opposite_price = self.book.best_price(side.opposite)        
+
+            if opposite_price is None:
+                break
+
+            # BUY  → match when offer <= limit price (BUY price)
+            # SELL → match when bid >= limit price (SELL Price)
+
+            if side is Side.BUY:
+                price_is_acceptable = opposite_price <= price
+                
+            else:
+                price_is_acceptable = opposite_price >= price
+
+            if not price_is_acceptable:
+                break
+
+            opposite_levels = self.book.offers if side is Side.BUY else self.book.bids
+
+            # Level where it's going to have the match
+            matching_level = opposite_levels[opposite_price]
+            level_trade_qty = 0
+
+            while order.qty > 0 and matching_level.first is not None: 
+
+                # Picking the first element
+                match = matching_level.first
+
+                trade_qty = min(order.qty, match.qty)
+
+                # When I subtract the lowest value from trade_qty, I'll have 0
+                order.qty -= trade_qty
+                match.qty -= trade_qty
+                matching_level.total_qty -= trade_qty
+
+                level_trade_qty += trade_qty
+
+                if match.qty == 0:
+                    matching_level.remove_first()
+                    del self.book.orders[match.order_id]
+
+            list_trades.append(Trade(opposite_price, level_trade_qty))    
+
+            # If the last matching level is empty
+            if matching_level.first is None:
+
+                self.book.remove_empty_level(side.opposite, opposite_price)
+
+        # If there's any left over inside order.qty
+        if order.qty > 0:
+
+            level_after_matching = self.book.get_or_create_level(side, price)
+            level_after_matching.last_insert(order)
+            self.book.orders[order.order_id] = order
+
+        return list_trades
+
+        
+
+        
+
+
+        
